a40552bf4f84406601555e4093979da382668466
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2014-08-09 10:43:11 +0200

    Move to named callbacks.

--- index.js
+++ index.js
@@ -4,28 +4,56 @@ function CountableSet(num, done) {
   this.num = num || 1
   this.done = done || noop
   this.finished = false
+  this.args = []
 
   return this.count.bind(this)
 }
 
-CountableSet.prototype.count = function count(err) {
-  var args = Array.prototype.slice.call(arguments, 0)
+CountableSet.prototype.count = function count(name) {
+  var self = this
 
-  this.num--
+  name = name || ''
 
-  if (this.finished) {
-    return false
+  return function(err) {
+    var args = Array.prototype.slice.call(arguments, 0)
+
+    self.num--
+    self.args.push([name].concat(args))
+
+    if (self.finished) {
+      return false
+    }
+
+    if (err) {
+      self.finished = true
+      return self.done.apply(self, args)
+    }
+
+    if (self.num === 0) {
+      self.finished = true
+      return self.done.apply(self, [null, self.format(self.args)])
+    }
   }
+}
 
-  if (err) {
-    this.finished = true
-    return this.done.apply(this, args)
-  }
+CountableSet.prototype.format = function format(args) {
+  var named = {}
 
-  if (this.num === 0) {
-    this.finished = true
-    return this.done.apply(this, args)
-  }
+  args.forEach(function(arg) {
+    if (arg.length < 2) {
+      return named[arg[0]] = undefined
+    }
+
+    return named[arg[0]] = arg.slice(2, 3).pop()
+  })
+
+  named.all = args.map(function(arg) {
+    return arg.slice(2)
+  })
+
+  delete named['']
+
+  return named
 }
 
 module.exports = function(num, done) {
--- test.js
+++ test.js
@@ -2,28 +2,50 @@ var test = require('tap').test
   , countn = require('./index')
   , noop = function() {}
 
-
-test('it should initialize and return count callback', function(t) {
+test('should initialize and return count callback', function(t) {
   t.type(countn(1, noop), 'function', 'callback is function')
   t.end()
 })
 
-test('it should call done when all callbacks are counted', function(t) {
+test('should call done when all callbacks are counted', function(t) {
   var cb = countn(2, function(err) {
     t.ok(true, 'all callbacks counted')
     t.end()
   })
 
-  cb()
-  cb()
+  cb()()
+  cb()()
 })
 
-test('it should return error on first new Error', function(t) {
+test('should return error on first error', function(t) {
   var cb = countn(3, function(err) {
     t.equal(err.message, 'Some error occured')
     t.end()
   })
 
-  cb(new Error('Some error occured'))
-  cb(new Error('Some other error'))
+  cb()(new Error('Some error occured'))
+  cb()(new Error('Some other error'))
+})
+
+test('done cb should pass named results and single value', function(t) {
+  var cb = countn(2, function(err, results) {
+    t.equal(results.first, true, 'first should be true')
+    t.equal(results.second, false, 'second should be false')
+    t.ok(true, 'all callbacks counted')
+    t.end()
+  })
+
+  cb('first')(null, true)
+  cb('second')(null, false)
+})
+
+test('done cb should pass all args', function(t) {
+  var cb = countn(2, function(err, results) {
+    t.deepEqual(results.all, [[true, false], [false, false]], 'all arguments should be merged')
+    t.ok(true, 'all callbacks should be counted')
+    t.end()
+  })
+
+  cb('first')(null, true, false)
+  cb('second')(null, false, false)
 })
