03c9ed97ed7c46a1e0516c2481686957a1352d7c
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2015-02-26 13:17:14 +0100

    Remove named functionality, go back to basic callback counting.

--- test.js
+++ test.js
@@ -1,20 +1,34 @@
+'use strict'
+
 var test = require('tap').test
   , countn = require('./index')
-  , noop = function() {}
 
 test('should initialize and return count callback', function(t) {
-  t.type(countn(1, noop), 'function', 'callback is function')
+  t.type(countn(1), 'function', 'callback is function')
   t.end()
 })
 
 test('should call done when all callbacks are counted', function(t) {
-  var cb = countn(2, function(err) {
+  var cb = countn(2, function() {
     t.ok(true, 'all callbacks counted')
     t.end()
   })
 
-  cb()()
-  cb()()
+  cb()
+  cb()
+})
+
+test('done callback should receive all results', function(t) {
+  var cb = countn(4, function(err, results) {
+    t.notOk(err, 'error should not be set')
+    t.equal(4, results.length, 'we should have all results included')
+    t.end()
+  })
+
+  cb(null, 1)
+  cb(null, 2)
+  cb(null, 3)
+  cb(null, 4)
 })
 
 test('should return error on first error', function(t) {
@@ -23,29 +37,6 @@ test('should return error on first error', function(t) {
     t.end()
   })
 
-  cb()(new Error('Some error occured'))
-  cb()(new Error('Some other error'))
-})
-
-test('done cb should pass named results and single value', function(t) {
-  var cb = countn(2, function(err, results) {
-    t.equal(results.first, true, 'first should be true')
-    t.equal(results.second, false, 'second should be false')
-    t.ok(true, 'all callbacks counted')
-    t.end()
-  })
-
-  cb('first')(null, true)
-  cb('second')(null, false)
-})
-
-test('done cb should pass all args', function(t) {
-  var cb = countn(2, function(err, results) {
-    t.deepEqual(results.all, [[true, false], [false, false]], 'all arguments should be merged')
-    t.ok(true, 'all callbacks should be counted')
-    t.end()
-  })
-
-  cb('first')(null, true, false)
-  cb('second')(null, false, false)
+  cb(new Error('Some error occured'))
+  cb(new Error('Some other error'))
 })
